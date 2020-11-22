97f6a16e2dd3c8b054d36c08fb3e1a9a8165ce1f
Nytelife26 <xtylerjrx@gmail.com>
2020-11-22 19:43:15 +0000

    feat(academic): finish fox-and-hen

--- works/academic/2020/computer-science/assignments/fox-and-hen/foxandhen.py
+++ works/academic/2020/computer-science/assignments/fox-and-hen/foxandhen.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 
 term = os.get_terminal_size().columns
 try:
@@ -32,9 +33,7 @@ def clearterm():
 
 def display(left, right):
 	side = (term - 3) // 2
-
 	clearterm()
-
 	left  = ["~ WRONG SIDE ~"] + left
 	right = ["~ RIGHT SIDE ~"] + right
 
@@ -61,47 +60,72 @@ def swap(left, right, entity):
 		left.append(right.pop(right.index("You")))
 		if entity in right:
 			left.append(right.pop(right.index(entity)))
-
 	return (left, right)
+
+def play(lside, rside):
+	display(lside, rside)
+	choices = {"f": "Fox", "h": "Hen", "c": "Corn", "n": None}
+	while True:
+		choice = input("Which do you wish to move? (F, H, C, or N for none)\n> ").lower()
+		if choice in choices.keys():
+			lside, rside = swap(lside, rside, choices[choice])
+			break
+		print("That was not one of the required choices. Please try again.")
+
+	fox = ["Fox" in lside, "Fox" in rside]
+	hen = ["Hen" in lside, "Hen" in rside]
+	corn = ["Corn" in lside, "Corn" in rside]
+	you = ["You" in lside, "You" in rside]
+
+	display(lside, rside)
+	if all([x == [False, True] for x in [fox, hen, corn]]):
+		return (True, True, lside, rside)
+	if fox == hen and fox != you:
+		print("You let the fox eat the hen... Better luck next time! :(")
+		return (True, False, lside, rside)
+	if hen == corn and hen != you:
+		print("You let the hen eat the corn... Better luck next time! :(")
+		return (True, False, lside, rside)
+	return (False, False, lside, rside)
 
 def game():
 	lside = ["You", "Fox", "Hen", "Corn"]
 	rside = []
-	over = False
 	moves = 0
-	while not over:
-		display(lside, rside)
-		choices = {"f": "Fox", "h": "Hen", "c": "Corn", "n": None}
-		while True:
-			choice = input("Which do you wish to move? (F, H, C, or N for none)\n> ").lower()
-			if choice in choices.keys():
-				lside, rside = swap(lside, rside, choices[choice])
-				break
-			print("That was not one of the required choices. Please try again.")
+	orig = time.time()
+	stats = getstats()
+	stats["gameplay"] += 1
+	while True:
 		moves += 1
+		turn = play(lside, rside)
+		if turn[0]:
+			elapsed = round(time.time() - orig, 2)
+			break
 
-		fox = ["Fox" in lside, "Fox" in rside]
-		hen = ["Hen" in lside, "Hen" in rside]
-		corn = ["Corn" in lside, "Corn" in rside]
-		you = ["You" in lside, "You" in rside]
-		if all([x == [False, True] for x in [fox, hen, corn]]):
-			display(lside, rside)
-			print(f"You won in {moves} moves! You moved them all to the right side of the river without losing. Well done!")
-			over = True
-		if fox == hen and fox != you:
-			display(lside, rside)
-			print("Ouch... You let the fox eat the hen, so you lose... Better luck next time! :(")
-			over = True
-		if hen == corn and hen != you:
-			display(lside, rside)
-			print("Ouch... You let the hen eat the corn, so you lose... Better luck next time! :(")
-			over = True
+	if turn[1]:
+		stats["gamewins"] += 1
+		print(f"You won in {moves} moves, and {elapsed} seconds! Well done!")
+		if moves < stats["bestmove"] or not stats["bestmove"]:
+			print("That's a new personal best!")
+			stats["bestmove"] = moves
+		if elapsed < stats["besttime"] or not stats["besttime"]:
+			print("Wow, a new best time! Congrats!")
+			stats["besttime"] = elapsed
+	recstats(stats)
 	input("Press enter to return to main menu...")
 
-
 def info():
-	clearterm()
-	pass
+	stats = getstats()
+	print(f"""
+You have played {stats["gameplay"]} games, \
+of which you have won {stats["gamewins"]}.
+
+Your best scores were {stats["bestmove"]} moves \
+and {stats["besttime"]} seconds.
+
+You may press return to exit this page at any time.
+	""")
+	input(">  ")
 
 def help():
 	print("""
@@ -143,6 +167,7 @@ def mainmenu():
 		if choice in choices.keys():
 			if choice is "4":
 				raise KeyboardInterrupt
+			clearterm()
 			choices[choice]()
 
 if __name__ == "__main__":
