48c171418b9811f907317f8aab54184a2cdee91e
Nytelife26 <xtylerjrx@gmail.com>
2020-12-06 22:40:42 +0000

    feat(personal): aoc day1 p1

---
+++ works/personal/code/2020/aoc/day1/p1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+def main():
+	nums = []
+	while True:
+		toadd = input("> ")
+		if toadd == "":
+			break
+		try:
+			nums.append(int(toadd))
+		except ValueError:
+			print("Invalid number. Please try again.")
+	for x in nums:
+		complement = 2020 - x
+		if complement in nums:
+			pair = (x, complement)
+	print(f"Result: {pair[0] * pair[1]}")
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		print("Exiting...")
