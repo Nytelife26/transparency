551dbbbc547f0e2b8570584983f39dc7eaff7077
Nytelife26 <xtylerjrx@gmail.com>
2021-01-19 22:38:59 +0000

    refactor(academic): clean up exercism bash phone-number

--- works/academic/2020/computer-science/independent-study/exercism/bash/phone-number/phone_number.sh
+++ works/academic/2020/computer-science/independent-study/exercism/bash/phone-number/phone_number.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-invalid() {
-	echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9"
-	exit 1
-}
-
 number=$(echo "$1" | sed -e 's/[^0-9]//g')
 if [[ "$number" =~ ^1?[2-9][0-9]{2}[2-9][0-9]{6}$ ]]; then
 	echo $(echo "$number" | sed -e 's/^1//')
 else
-	invalid
+	echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9"
+	exit 1
 fi
