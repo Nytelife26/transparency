5ff4d21244a9172268a335966042aa0181b737c9
Nytelife26 <xtylerjrx@gmail.com>
2021-05-06 12:48:54 +0100

    feat(academic): add exercism rust armstrong-numbers

--- works/academic/2020-2021/computer-science/independent-study/exercism/rust/armstrong-numbers/src/lib.rs
+++ works/academic/2020-2021/computer-science/independent-study/exercism/rust/armstrong-numbers/src/lib.rs
@@ -1,8 +1,4 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-	let digits = num.to_string().chars().collect::<Vec<char>>();
-	let length = digits.len() as u32;
-
-	num == digits.iter().fold(0, |a, b| {
-		a + str::parse::<u32>(&b.to_string()).unwrap().pow(length)
-	})
+	let length = ((num as f32).log10() + 1f32).floor() as u32;
+	num == (0..length).fold(0, |a, b| a + (num / 10u32.pow(b) % 10).pow(length))
 }
