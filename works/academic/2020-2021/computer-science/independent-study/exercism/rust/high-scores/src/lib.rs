cd0857142728b3d83a20705c32477c662663ee9f
Nytelife26 <xtylerjrx@gmail.com>
2021-05-06 16:01:22 +0100

    feat(academic): add exercism rust high-scores

---
+++ works/academic/2020-2021/computer-science/independent-study/exercism/rust/high-scores/src/lib.rs
@@ -0,0 +1,28 @@
+pub struct HighScores<'a> {
+	scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+	pub fn new(scores: &'a [u32]) -> Self {
+		HighScores { scores }
+	}
+
+	pub fn scores(&self) -> &'a [u32] {
+		self.scores
+	}
+
+	pub fn latest(&self) -> Option<u32> {
+		self.scores.last().cloned()
+	}
+
+	pub fn personal_best(&self) -> Option<u32> {
+		self.scores.iter().max().cloned()
+	}
+
+	pub fn personal_top_three(&self) -> Vec<u32> {
+		let mut result: Vec<u32> = self.scores.to_vec();
+		result.sort_unstable_by(|a, b| b.cmp(a));
+		result.truncate(3);
+		result
+	}
+}
