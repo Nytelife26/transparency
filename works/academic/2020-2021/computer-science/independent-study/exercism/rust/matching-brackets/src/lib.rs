0858b6edc3a0139097154059c2a392988522cda9
Nytelife26 <xtylerjrx@gmail.com>
2021-05-07 11:30:19 +0100

    feat(academic): add exercism rust matching-brackets

--- works/academic/2020-2021/computer-science/independent-study/exercism/rust/matching-brackets/src/lib.rs
+++ works/academic/2020-2021/computer-science/independent-study/exercism/rust/matching-brackets/src/lib.rs
@@ -1,30 +1,13 @@
-const BRACKETS: &'static [char] = &['[', '{', '(', ')', '}', ']'];
-
-pub fn brackets(x: &char) -> bool {
-	BRACKETS.contains(x)
-}
-
 pub fn brackets_are_balanced(string: &str) -> bool {
 	let mut nest: Vec<char> = Vec::new();
-	string.chars().filter(brackets).for_each(|x| {
-		let item = match nest.last() {
-			Some(val) => val,
-			None => &' ',
-		};
+	for x in string.chars() {
 		match x {
-			')' if item == &'(' => {
-				nest.pop();
-			}
-			']' if item == &'[' => {
-				nest.pop();
-			}
-			'}' if item == &'{' => {
-				nest.pop();
-			}
-			_ => {
-				nest.push(x);
-			}
+			'[' | '{' | '(' => nest.push(x),
+			']' => if nest.pop() != Some('[') { return false; },
+			'}' => if nest.pop() != Some('{') { return false; },
+			')' => if nest.pop() != Some('(') { return false; },
+			_ => (),
 		};
-	});
+	}
 	nest.len() == 0
 }
